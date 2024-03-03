1509da44aedc3696d9fdf79a12abbd7cc493dddd
こた <tokyosato1@gmail.com>
2024-03-03 21:04:04 +0900

    Add Header, Footer, and DisclaimerModal components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,39 @@
+import { Box, Button, HStack, Link } from '@chakra-ui/react';
+
+const Footer = ({ onOpen }: { onOpen: () => void }) => {
+    return (
+        <Box mt={20} py={4} borderColor='#FFF' borderTopWidth='1px' bg='#161918'>
+            <HStack spacing={4} justifyContent='flex-start' pl={4}>
+                <Link
+                    href='https://hi-there-this-is-kota.vercel.app'
+                    isExternal
+                    color='#FFF'
+                    fontSize='sm'
+                    _hover={{ color: '#00c8ff' }}
+                >
+                    Developer&apos;s Website
+                </Link>
+                <Link
+                    href='https://github.com/07130918/EmailForge/issues/new'
+                    isExternal
+                    color='#FFF'
+                    fontSize='sm'
+                    _hover={{ color: '#00c8ff' }}
+                >
+                    Report a Bug
+                </Link>
+                <Button
+                    color='#FFF'
+                    variant='ghost'
+                    size='sm'
+                    _hover={{ color: '#00c8ff' }}
+                    onClick={onOpen}
+                >
+                    免責事項/Disclaimer
+                </Button>
+            </HStack>
+        </Box>
+    );
+};
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,26 @@
+import { HStack, Text } from '@chakra-ui/react';
+
+const Header = () => {
+    return (
+        <HStack
+            as='header'
+            width='full'
+            bgGradient='linear(135deg, #00c8ff, #1f7bc2, #00155c)'
+            p={4}
+            justifyContent='space-between'
+            alignItems='center'
+            position='fixed'
+            top={0}
+            zIndex='banner'
+        >
+            <Text fontSize='xl' color='white' fontWeight='bold' pl={5}>
+                EmailForge
+            </Text>
+            <Text color='white' pr={5}>
+                Creating awesome emails are not rocket science 🚀
+            </Text>
+        </HStack>
+    );
+};
+
+export default Header;
